0a0fd440481ded55c420a63a1f1ececfef82345c
Ingrid <ingrita.rpm@gmail.com>
2019-11-14 22:56:33 -0600

    body parser

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express=require('express')
 const app = express();
 const mysql = require('mysql');
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 var connection;
 function connect() {
@@ -24,6 +27,21 @@ app.get('/bd', (req,res)=>{
     });
     console.log("Conectado siuuu");
     connection.end();
+});
+
+app.post('/checkout', function (req, res) {
+	/*
+	nombre
+	apellidos
+	correoElectronico
+	direccion
+	nombreTarjeta
+	numeroTarjeta
+	expiracion
+	cvv
+	*/
+	
+  res.send('El nombre es: ' + req.body.nombre)
 });
 
 
