e6ba5bbc5570e0be68e957a95e0d57ce810a10e3
Ingrid <ingrita.rpm@gmail.com>
2019-11-15 01:03:45 -0600

    Registrar compra

--- index.js
+++ index.js
@@ -29,21 +29,79 @@ app.get('/bd', (req,res)=>{
     connection.end();
 });
 
-app.post('/checkout', function (req, res) {
-	/*
-	nombre
-	apellidos
-	correoElectronico
-	direccion
-	nombreTarjeta
-	numeroTarjeta
-	expiracion
-	cvv
-	*/
-	
-  res.send('El nombre es: ' + req.body.nombre)
+app.get('/factura', (req,res)=>{
+    console.log("GET init");
+    connect();
+    connection.query("SELECT * FROM factura", function (err, result, fields) {
+      if (err) {
+        console.log("Error en el select", err);
+      }
+      res.send(result)
+    });
+    console.log("Conectado siuuu");
+    connection.end();
 });
 
+app.get('/detalle_factura', (req,res)=>{
+    console.log("GET init");
+    connect();
+    connection.query("SELECT * FROM detalle_factura", function (err, result, fields) {
+      if (err) {
+        console.log("Error en el select", err);
+      }
+      res.send(result)
+    });
+    console.log("Conectado siuuu");
+    connection.end();
+});
+
+app.post('/checkout', function (req, res) {	
+	var insertar = "INSERT INTO factura (nombre,correo,direccion,nombre_en_la_tarjeta,numero_de_tarjeta,expiracion,cvv) VALUES ('"+req.body.nombre+" "+req.body.apellidos+"','"+req.body.correo+"','"+req.body.direccion+"','"+req.body.nombre_en_la_tarjeta+"'," +req.body.numero_de_tarjeta+",'"+"1-"+req.body.expiracion+"',"+req.body.cvv+")";
+	connect();
+	connection.query(insertar, function (err, result, fields) {
+      if (err) {
+        console.log("Error en la inserción de la factura", err);
+      }
+    });
+	connection.end();
+	var id_factura = -1;
+	connect();
+	connection.query("Select id from factura order by id desc", function (err, result, fields) {
+      if (err) {
+        console.log("Error en la busqueda de id factura", err);
+      }
+	  id_factura = result[0].id;
+	  agregar_productos(req.body.detalle_factura, id_factura);
+	  res.send(""+id_factura);
+    });
+});
+
+function agregar_productos(detalle_factura, id_factura){
+	var detalle = detalle_factura.split(","); 
+	var productos = [];
+	var ids = [];
+	var contador = 0;
+	detalle.forEach(element => { 
+	if(typeof productos[element+"-"] === 'undefined'){
+		productos[element+"-"] = 1;
+		ids[contador] = element;
+		contador++;
+	} else {
+		productos[element+"-"] += 1;
+	}
+	}); 
+	contador = 0;
+	ids.forEach(element => { 
+	console.log(element + "-" + productos[element+"-"]);
+	connect();
+	connection.query("Insert into detalle_factura values ("+id_factura+","+element+","+productos[element+"-"]+")", function (err, result, fields) {
+      if (err) {
+        console.log("Error en la inserción del detalle_factura", err);
+      }
+    });
+	contador++;
+	}); 
+}
 
 app.listen(3000,()=>{
   console.log('funciona');
